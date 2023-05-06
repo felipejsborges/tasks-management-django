19d50b40ea1ab7dfc83e388af1ccb46815c69149
Felipe Borges <felipejsborges@outlook.com>
2023-05-06 16:17:13 -0300

    allow to completed a task

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("tasks", views.taskListAPIView),
     path("tasks/<int:pk>", views.taskDetailAPIView),
+    path("tasks/<int:pk>/complete", views.taskCompletingAPIView),
     path("users", views.userCreateAPIView),
     path("users/profile", views.userProfileAPIView),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
@@ -5,6 +7,20 @@ from rest_framework.views import APIView
 
 from .models import Task, User
 from .serializers import TaskSerializer, UpdateProfileSerializer, UserSerializer
+
+
+def get_task(pk):
+    try:
+        return Task.objects.get(pk=pk)
+    except Task.DoesNotExist as err:
+        raise Http404 from err
+
+
+def get_user(pk):
+    try:
+        return User.objects.get(pk=pk)
+    except User.DoesNotExist as err:
+        raise Http404 from err
 
 
 class UserCreate(APIView):
@@ -33,21 +49,15 @@ class UserProfile(APIView):
     Retrieve the current user.
     """
 
-    def get_object(self, pk):
-        try:
-            return User.objects.get(pk=pk)
-        except User.DoesNotExist as err:
-            raise Http404 from err
-
     def get(self, request):
         pk = request.user.id
-        user = self.get_object(pk)
+        user = get_user(pk)
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
     def put(self, request):
         pk = request.user.id
-        user = self.get_object(pk)
+        user = get_user(pk)
         serializer = UpdateProfileSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -84,19 +94,13 @@ class TaskDetail(APIView):
     Retrieve, update or delete a task instance.
     """
 
-    def get_object(self, pk):
-        try:
-            return Task.objects.get(pk=pk)
-        except Task.DoesNotExist as err:
-            raise Http404 from err
-
     def get(self, _, pk):
-        task = self.get_object(pk)
+        task = get_task(pk)
         serializer = TaskSerializer(task)
         return Response(serializer.data)
 
     def put(self, request, pk):
-        task = self.get_object(pk)
+        task = get_task(pk)
         serializer = TaskSerializer(task, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -104,9 +108,27 @@ class TaskDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, _, pk):
-        task = self.get_object(pk)
+        task = get_task(pk)
         task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 taskDetailAPIView = TaskDetail.as_view()
+
+
+class TaskCompleting(APIView):
+    """
+    Complete a task.
+    """
+
+    def patch(self, request, pk):
+        task = get_task(pk)
+        task.completed_at = datetime.now()
+        serializer = TaskSerializer(task, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+taskCompletingAPIView = TaskCompleting.as_view()
